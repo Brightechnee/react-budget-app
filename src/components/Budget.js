5fee15f7ea5740d561d4faa8d9a171c2a302cfcd
Bright Mahere <111682296+Brightechnee@users.noreply.github.com>
2024-02-09 21:33:56 +0530

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,17 +2,35 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
-    return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
-    );
-};
-export default Budget;
+  const { budget, dispatch } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
 
+  const handleBudgetChange = (event) => {
+    setNewBudget(event.target.value);
+  }
+
+  const updateBudget = () => {
+    // Use getElementById to get the input element and set the new budget
+    const inputElement = document.getElementById('budgetInput');
+    if (inputElement) {
+      const inputValue = inputElement.value;
+      dispatch({ type: 'SET_BUDGET', payload: parseInt(inputValue, 10) });
+    }
+  }
+
+  return (
+    <div className='alert alert-secondary'>
+      <span>Budget: £{budget}</span>
+      <input
+        id="budgetInput"
+        type="number"
+        step="10"
+        value={newBudget}
+        onChange={handleBudgetChange}
+        onBlur={updateBudget}
+      />
+    </div>
+  );
+};
+
+export default Budget;
